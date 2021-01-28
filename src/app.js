7a1f99c9b4c21f4a1f98b657e0e7afae85fac0e7
tissarni <tissarni>
2021-01-28 11:32:32 +0100

    end of full DH exchange

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
+/*
+
+[
+    "channel_publicKey": "...",
+    "members": [
+        "bob": [
+            "publicKey": "...",
+            "encrypted_channel_privateKey": "..."
+        ],
+        "alice": [
+            "publicKey": "...",
+            "encrypted_channel_privateKey": "..."
+        ],
+        ...
+    ]
+]
+
+Step1. Alice alone
+- channel_publicKey
+- Alice publicKey
+- Alice encrypted channel privateKey
+
+Step2. Bob want to join
+- channel_publicKey
+- Alice publicKey
+- Alice encrypted channel privateKey
+- Bob publicKey
+- There is no Bob encrypted channel privateKey
+
+Step3. Alice want to send a message
+- channel_publicKey
+- Alice publicKey
+- Alice encrypted channel privateKey
+- Bob publicKey
+- Bob encrypted channel privateKey
+
+*/
+
+
 const { Client } = require('./client');
 const {createLink, encrypt, decrypt} = require('./cryptographie');
 const { Server } = require('./server');
@@ -9,17 +48,31 @@ const alice = new Client("1243538", "alice");
 const titou = new Client("1243538", "titou");
 
 
+
+
+twakeChannel.socketConnect(bob, (message) => {
+    console.log("Bob received: ", message);
+});
+
+twakeChannel.socketConnect(alice, (message) => {
+    console.log("Alice received: ", message);
+});
+
+twakeChannel.socketConnect(titou, (message) => {
+    console.log("Titou received: ", message);
+});
+
+twakeChannel.socketSend(bob, "message sent by socket");
+
+console.log("Bob http result", twakeChannel.httpSend("message:send", "message sent by http"));
+
+
+
 createLink(bob, alice, titou, twakeChannel);
-//console.log(twakeChannel.member);
+
 
 const message = "Twake E2EE is comming";
-/*console.log(message);
-const encryptedMessageforBob = encrypt(message, Buffer.from(alice.sharedKey1 ,'hex'));
-const encryptedMessageforTitou = encrypt(message, Buffer.from(alice.sharedKey2 ,'hex'));
-const decryptedMessageByBob = decrypt(encryptedMessageforBob, Buffer.from(bob.sharedKey1,'hex'));
-const decryptedMessageByTitou = decrypt(encryptedMessageforTitou, Buffer.from(titou.sharedKey2,'hex'));
-console.log("bob: ", decryptedMessageByBob);
-console.log("titou: ", decryptedMessageByTitou);*/
+
 
 console.log("envoyer coté alice: ", message);
 alice.send(message, twakeChannel, bob, titou);
@@ -28,6 +81,6 @@ bob.receive(twakeChannel, alice);
 titou.receive(twakeChannel, alice);
 console.log("recu chez bob: ", bob.received);
 console.log("recu chez titou: ",titou.received);
-//console.log(twakeChannel)
+
 
 
--- src/server.js
+++ src/server.js
@@ -19,6 +19,37 @@ class Server {
         receiver1.receive(this, sender);
         receiver2.receive(this.sender)
     }
+
+    httpSend(action, body){
+
+        if(action === "message:send"){
+            this.socketSend(null, body);
+            return "ok, http sent";
+            //Some code that represent sending a message to the server
+        }
+
+    }
+
+    socketClients = [];
+
+    socketConnect(client, callback){
+        this.socketDisconnect(client);
+        this.socketClients.push({client: client, callback: callback});
+    }
+
+    socketDisconnect(client){
+        this.socketClients = this.socketClients.filter((c)=>c.client !== client);
+    }
+
+    socketSend(client, body){
+        if(!client || this.socketClients.filter((c)=>c.client === client).length > 0){
+            this.socketClients.forEach((c)=>{
+                c.callback(body);
+            });
+        }
+    }
+
+    
 }
 
 
